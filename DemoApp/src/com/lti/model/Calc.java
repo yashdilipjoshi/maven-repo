634a07afafc87e48bdf9375d98301939a2b3b2ae
Yash <Yash@DESKTOP-8E8NFE2>
2020-12-18 16:28:19 +0530

    modified add method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -2,7 +2,7 @@ package com.lti.model;
 
 public class Calc {
 	public void add(int num1, int num2) {
-		System.out.println("sum= "+(num1-num2));
+		System.out.println("summation= "+num1+num2);
 	}
 	public void diff(int num1, int num2) {
 		System.out.println("diff= "+(num1-num2));

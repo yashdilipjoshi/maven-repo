7b1f077c766ddda680bd75cedf6e67583689f28e
yashdilipjoshi <yashdilipjoshi@gmail.com>
2020-12-21 12:43:57 +0530

    first commit

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,15 @@
+package com.lti;
+
+public class Calc {
+	
+	public static int add(int num1, int num2) {
+		return num1+num2;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(add(10,20));
+
+	}
+
+}
